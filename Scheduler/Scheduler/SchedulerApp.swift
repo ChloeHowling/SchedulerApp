290614ce339c8a0a700f18f546b58067fe915ea8
Chloe Howling <chloehltam@gmail.com>
2021-10-04 19:25:17 -0600

    Initial commit

---
+++ Scheduler/Scheduler/SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulerApp.swift
+//  Scheduler
+//
+//  Created by Chloe Tam on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
